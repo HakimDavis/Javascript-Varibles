de947af89e4d64407ec9acf3dac6f44723a8b15a
HakimDavis <Hakimdavis2000@gmail.com>
2016-05-09 08:56:06 -0400

    javascript variables

---
+++ app.js
@@ -0,0 +1,17 @@
+var hello =  "ALERT";
+alert(hello);
+
+var qwertyuiop = "asdjkl"
+console.log(qwertyuiop)
+
+var zxcvbnm = "1234567890"
+document.write(zxcvbnm)
+// var subjects=["math", "reading","anything"]
+// console.log(subjects[2]);
+// var subjects=["math", "utdyrtfjyfdryhjy", "thhoshhdo"]
+// console.log(subjects[1]);
+// console.log= (subjects.length)
+// for(var i = 0; i <= subjects.length - 1; i++)
+// 	{
+// 		console.log(i + " " + subjects[i])
+// 	}
